e1c3d2420e7b1e1a07c239909141e737be3539c8
pmtip <2932364151@qq.com>
2021-01-22 23:03:33 +0800

    Update.

--- utils.c
+++ utils.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 #include<sys/stat.h>
 #include"loader.h"
-int is_exec(const char *filename)
+int
+is_exec (const char *filename)
 {
-    struct stat buf;
-    FILE *f;
-    if (stat(filename, &buf) == -1)
-        return 0;
-    if (!S_ISREG(buf.st_mode) || (buf.st_mode & S_IXUSR) != S_IXUSR)
-        return 0;
-    if (!(f = fopen(filename, "rb")))
-        return 0;
-    if (!check_elf(f))
-        return 0;
-    return 1;
+  struct stat buf;
+  FILE *f;
+
+  if (stat (filename, &buf) == -1)
+    return 0;
+  if (!S_ISREG (buf.st_mode) || (buf.st_mode & S_IXUSR) != S_IXUSR)
+    return 0;
+  if (!(f = fopen (filename, "rb")))
+    return 0;
+  if (!check_elf (f))
+    return 0;
+  return 1;
 }
