72e8a45664c7d1ba6ff0d7aa16cc79be194893d9
pmtip <2932364151@qq.com>
2021-01-23 00:24:53 +0800

    Update.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<elf.h>
 #include"utils.h"
 #include"loader.h"
 int main(int argc, char **argv, char **envp)
@@ -10,13 +11,18 @@ int main(int argc, char **argv, char **envp)
     char *filename = (char *)malloc(strlen(argv[1]));
 
     strcpy(filename, argv[1]);
-    FILE *f = fopen(filename, "rb");
+    FILE *f;
+    void *entry = NULL;
     int e_argc = argc - 1;
     char **e_argv = argv + 1;
 
     if (!is_exec(filename))
         exit(1);
+    f = fopen(filename, "rb");
     if (!load_elf(f))
         exit(1);
-    while (1) ;
+    Elf32_Ehdr ehdr;
+
+    fread(&ehdr, sizeof(Elf32_Ehdr), 1, f);
+    entry = ehdr.e_entry;
 }
