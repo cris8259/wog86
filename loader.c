705f881ae3bb49f88ba2e361dc937bcc16970063
pmtip <2932364151@qq.com>
2021-01-22 23:46:58 +0800

    Update.

--- loader.c
+++ loader.c
@@ -6,81 +6,78 @@
 #include<sys/mman.h>
 #include"loader.h"
 
-#define PAGESIZE 4096
-
-int
-check_elf (FILE * f)
+int check_elf(FILE * f)
 {
-  Elf32_Ehdr header;
+    Elf32_Ehdr header;
 
-  if (fread (&header, sizeof (Elf32_Ehdr), 1, f) != 1)
-    return 0;
-  if (memcmp (header.e_ident, ELFMAG, SELFMAG) != 0
-      || header.e_ident[EI_CLASS] != ELFCLASS32
-      || header.e_ident[EI_DATA] != ELFDATA2LSB
-      || header.e_ident[EI_VERSION] != EV_CURRENT
-      || (header.e_ident[EI_OSABI] != ELFOSABI_LINUX
-	  && header.e_ident[EI_OSABI] != ELFOSABI_NONE
-	  && header.e_ident[EI_OSABI] != ELFOSABI_SYSV)
-      || header.e_ehsize != sizeof (Elf32_Ehdr)
-      || header.e_phentsize != sizeof (Elf32_Phdr)
-      || (header.e_type != ET_EXEC && header.e_type != ET_DYN)
-      || header.e_machine != EM_386)
-    return 0;
-  return 1;
+    if (fread(&header, sizeof(Elf32_Ehdr), 1, f) != 1)
+        return 0;
+    if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0
+        || header.e_ident[EI_CLASS] != ELFCLASS32
+        || header.e_ident[EI_DATA] != ELFDATA2LSB
+        || header.e_ident[EI_VERSION] != EV_CURRENT
+        || (header.e_ident[EI_OSABI] != ELFOSABI_LINUX
+            && header.e_ident[EI_OSABI] != ELFOSABI_NONE
+            && header.e_ident[EI_OSABI] != ELFOSABI_SYSV)
+        || header.e_ehsize != sizeof(Elf32_Ehdr)
+        || header.e_phentsize != sizeof(Elf32_Phdr)
+        || (header.e_type != ET_EXEC && header.e_type != ET_DYN)
+        || header.e_machine != EM_386)
+        return 0;
+    return 1;
 }
 
-int
-load_elf (FILE * f)
+int load_elf(FILE * f)
 {
-  Elf32_Ehdr ehdr;
-  Elf32_Phdr phdr;
-  int fd = fileno (f);
+    Elf32_Ehdr ehdr;
+    Elf32_Phdr phdr;
+    int fd = fileno(f);
 
-  if (fread (&ehdr, sizeof (Elf32_Ehdr), 1, f) != 1)
-    return 0;
-  if (fseek (f, ehdr.e_phoff, SEEK_SET) != 0)
-    return 0;
-  int i;
+    if (fread(&ehdr, sizeof(Elf32_Ehdr), 1, f) != 1)
+        return 0;
+    if (fseek(f, ehdr.e_phoff, SEEK_SET) != 0)
+        return 0;
+    int i;
 
-  for (i = 0; i < ehdr.e_phnum; i++)
+    for (i = 0; i < ehdr.e_phnum; i++)
     {
-      if (fread (&phdr, sizeof (Elf32_Phdr), 1, f) != 1)
-	{
-	  return 0;
-	}
-      if (phdr.p_type == PT_LOAD)
-	{
-	  int prot;
+        if (fread(&phdr, sizeof(Elf32_Phdr), 1, f) != 1)
+        {
+            return 0;
+        }
+        if (phdr.p_type == PT_LOAD)
+        {
+            int prot;
 
-	  if (phdr.p_flags & PF_R)
-	    prot = PROT_READ;
-	  if (phdr.p_flags & PF_W)
-	    if (!prot)
-	      prot = PROT_WRITE;
-	    else
-	      prot |= PROT_WRITE;
-	  if (phdr.p_flags & PF_X)
-	    if (!prot)
-	      prot = PROT_EXEC;
-	    else
-	      prot |= PROT_EXEC;
-	  if (mmap
-	      ((void *) (phdr.p_vaddr & ~(PAGESIZE - 1)),
-	       phdr.p_filesz, prot,
-	       MAP_PRIVATE | (ehdr.e_type == ET_EXEC ? MAP_FIXED : 0), fd,
-	       phdr.p_offset - (phdr.p_vaddr & (PAGESIZE - 1))) == MAP_FAILED)
-	    return 0;
-	  if (phdr.p_filesz < phdr.p_memsz)
-	    {
-	      if (mmap
-		  ((void *) ((phdr.p_vaddr + phdr.p_filesz) &
-			     ~(PAGESIZE - 1)), phdr.p_memsz - phdr.p_filesz,
-		   prot, MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE, -1,
-		   0) == MAP_FAILED)
-		return 0;
-	    }
-	}
+            if (phdr.p_flags & PF_R)
+                prot = PROT_READ;
+            if (phdr.p_flags & PF_W)
+                if (!prot)
+                    prot = PROT_WRITE;
+                else
+                    prot |= PROT_WRITE;
+            if (phdr.p_flags & PF_X)
+                if (!prot)
+                    prot = PROT_EXEC;
+                else
+                    prot |= PROT_EXEC;
+            if (mmap
+                ((void *)(phdr.p_vaddr & ~(PAGE_SIZE - 1)),
+                 phdr.p_filesz + PAGESIZE - 1, prot,
+                 MAP_PRIVATE | (ehdr.e_type == ET_EXEC ? MAP_FIXED : 0), fd,
+                 phdr.p_offset - (phdr.p_vaddr & (PAGE_SIZE - 1))) ==
+                MAP_FAILED)
+                return 0;
+            if (phdr.p_filesz < phdr.p_memsz)
+            {
+                if (mmap
+                    ((void *)((phdr.p_vaddr + phdr.p_filesz + PAGESIZE - 1) &
+                              ~(PAGE_SIZE - 1)), phdr.p_memsz - phdr.p_filesz,
+                     prot, MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE, -1,
+                     0) == MAP_FAILED)
+                    return 0;
+            }
+        }
     }
-  return 1;
+    return 1;
 }
--- main.c
+++ main.c
@@ -3,21 +3,20 @@
 #include<string.h>
 #include"utils.h"
 #include"loader.h"
-int
-main (int argc, char **argv, char **envp)
+int main(int argc, char **argv, char **envp)
 {
-  if (argc < 2)
-    exit (1);
-  char *filename = (char *) malloc (strlen (argv[1]));
+    if (argc < 2)
+        exit(1);
+    char *filename = (char *)malloc(strlen(argv[1]));
 
-  strcpy (filename, argv[1]);
-  FILE *f = fopen (filename, "rb");
-  int e_argc = argc - 1;
-  char **e_argv = argv + 1;
+    strcpy(filename, argv[1]);
+    FILE *f = fopen(filename, "rb");
+    int e_argc = argc - 1;
+    char **e_argv = argv + 1;
 
-  if (!is_exec (filename))
-    exit (1);
-  if (!load_elf (f))
-    exit (1);
-  while (1);
+    if (!is_exec(filename))
+        exit(1);
+    if (!load_elf(f))
+        exit(1);
+    while (1) ;
 }
